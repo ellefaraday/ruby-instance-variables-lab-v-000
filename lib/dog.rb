3b63e74a69a984e27b0b7a8c725e367749826ade
Elle Faraday <faraday.elle@gmail.com>
2017-11-08 03:37:52 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,6 +2,7 @@ class Dog
 
   def initialize(dog_name)
     @this_dogs_name = dog_name
+  end
 
   def name= (dog_name)
     @this_dogs_name = dog_name
@@ -10,5 +11,5 @@ class Dog
   def name
     @this_dogs_name
   end
-  
+
 end
